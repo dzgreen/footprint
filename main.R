c4e2b7e9580f06d5bfb15a0b92ccb63fa8316963
Dominik <dominik2711@gmail.com>
2017-08-28 14:55:28 +0200

    changed the structure of the document and allocate the tasks to all

--- main.R
+++ main.R
@@ -1,60 +1,19 @@
-# Analysis plan: 2016 Global Ecological Footprint
-# Does your country consume more resources than it produces in a year?
-# 
-# 
-#### Research question: ####
-#
-# Inspiration:
-# Is your country running an ecological deficit, consuming more resources than it can produce per year? 
-# Which countries have the greatest ecological deficits or reserves? 
-# Do they consume less or produce more than the average country? 
-# When will Earth Overshoot Day, the day on the calendar when humanity has used one year of natural resources,
-# occur in 2017?
-# 
-# 
-#### Hypotheses: ####
-#   Have at least one hypothesis for each major analyses undertaken and 
-#   several hypotheses for each multivariate regression analysis 
-# 
-# -	
-# -	
-#   
-####   Data cleaning process: ####
-#   -	Transformation of variables?
-# -	
-#   
-####   Definition and construction of the following: ####
-# -	Main exposure variables (if relevant)
-# -	Outcome variables 
-# -	Other covariates including potential confounders and mediators 
-# -	Definition of groups if planning to examine variations between sub-groups of respondents 
-# -	Procedures used for dealing with missing data
-#     o	Complete case analysis 
-#     o	Coding missing values as separate categories 
-#     o	Imputation/extrapolation methods 
-# 
-#### Data exploration: ####
-#   -	Outliers (Y – X) – boxplot and Cleavland dotplot 
-#   -	Homogeneity(X)–Conditional boxplot
-#   -	Normality (Y) – histogram or QQ plot
-#   -	Zero over dispersion (Y) – Frequency plot or corrgram
-#   -	Collinearity (X) – VIF, scatterplots, correlations and PCA
-#   -	Relationships (Y and X) – scatterplots, conditional boxplot
-#   -	Interactions – coplots
-#   -	Independence (Y) – ACF and variogram, plot Y vs. time/space 
-# 
-# 
-#### Analytical approach: ####
-#   Planned analysis:
-#     -	Statistical tests for testing different hypotheses
-# -	Planned tables and figures
-# -	Preliminary/basis analysis model
-# 
-
-
-
-
-
+#### Outline ####
+# Set working directory and read in data (Dominik)
+# Data inspection (Dominik)
+  # types of variables 
+  # NA's
+  # Outliers
+  # Scatterplots
+# Data cleaning (Dominik)
+# Data visualisation (All)
+  # 1 ) What is the distribution of the ecological footprint in the world? (Gary)
+  # 1a) Histograms 
+  # 1b) Map
+  # 2 ) what is the relationship between income and HDI and ecological foorprint in the world? (Louise)
+  # 2a) Scatterplot, correlation coefficient
+  # 3 ) What countries have an ecological debt and what countries have surplus? (Dominik)
+  # 3a) Map
 
 
 #### Set working directory ####
@@ -62,29 +21,21 @@ path_to_data <- "/home/dominik/Dropbox/Kandidat/Managing_big/datagroup_exam/"
 # ""
 # ""
 
-# Added thius line to test commit
 
 #### Read data ####
 countries <- read.csv(file = "countries.csv")
 
 
-#### First look at the data ####
+#### Data inspection ####
 dim(countries) # rows and columns
 str(countries) # variable and their mode.
 
-#### Changing mode of some variables ####
-# Let's convert `Country` to a character and `GDP.per.Capita` to numeric. Notice that the GDP variable has a $ sign we need to eliminate. 
+
+#### Changing type of some variables ####
 countries$Country <- as.character(countries$Country)
-library("stringr")
 countries$GDP.per.Capita <- as.character(countries$GDP.per.Capita)
-noget <- gsub(x = countries$GDP.per.Capita[1], pattern = "$", replacement = "")
-gsub(x = noget, pattern = "$", replacement = "")
-?gsub
+countries$GDP.per.Capita <- gsub(x = countries$GDP.per.Capita, pattern = "[$]", replacement = "")
+countries$GDP.per.Capita <- gsub(x = countries$GDP.per.Capita, pattern = "[,]", replacement = "")
+countries$GDP.per.Capita <- as.numeric(countries$GDP.per.Capita)
 
 
-#### New plot #####
-
-exciting stuff
-
-
-#### dgs ####
